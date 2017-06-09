18b567be02496213874e952dc10dd1f06a5a0709
xsunrtr <xsun@renttherunway.com>
2017-06-09 15:16:41 -0400

    set filter 2017 to next_updated_on instead of cur_updated_on

--- inventory_report_aging_daily.sql
+++ inventory_report_aging_daily.sql
@@ -19,12 +19,12 @@ DROP TABLE IF EXISTS temp1;
 create table temp1 as 
 select temp.* 
 , DATEDIFF(hour, temp.cur_updated_on, temp.next_updated_on) as time_to_next_scan
+, ius.seq as last_seq
 from temp
 left join etl.inventory_unit_seq as ius
 on temp.id = ius.unit_id
-where cur_location_site not in ('CUSTOMER','RACK') 
-and next_updated_on >= (cur_updated_on+ interval '48 HOUR')
-and temp.cur_updated_on >='2017-01-01';
+where next_updated_on >= (cur_updated_on+ interval '48 HOUR')
+and temp.next_updated_on >='2017-01-01';
 
 /*Join rtrbi.dates table to get each single date within aging period of each unit*/
 DROP TABLE IF EXISTS temp2;
@@ -39,10 +39,10 @@ left join rtrbi.dates as dates
 on (dates.asofdate + interval '10 HOUR') >= temp1.aging_date
 and (dates.asofdate + interval '10 HOUR') <= temp1.next_updated_on;
 
-select distinct(time_to_next_scan) from temp2 order by time_to_next_scan ASC;
+
 /*Summarize target table by asofdate, location_station and aging days bins*/
 select 
-count(*),
+count(*) as total,
 count(CASE WHEN time_to_next_scan>=48 AND time_to_next_scan < 72 THEN 1 END) AS '2-3 days',
 count(CASE WHEN time_to_next_scan>=72 AND time_to_next_scan<240 THEN 1 END) AS '3-10 days',
 count(CASE WHEN time_to_next_scan>=240 AND time_to_next_scan<720 THEN 1 END) AS '10-30 days',
@@ -54,8 +54,16 @@ where asofdate is not null
 group by asofdate,cur_location_station
 order by asofdate,cur_location_station;
 
+select count(*)
+,asofdate
+from temp2
+where asofdate is not null
+and asofdate >'2017-01-01'
+group by asofdate
+order by asofdate;
+
+
 drop table if exists temp;
 drop table if exists temp1;
-
 
 
