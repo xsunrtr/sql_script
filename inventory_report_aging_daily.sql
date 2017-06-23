5944c857fccf649edc6fbc3d98fc173e40004c20
xsunrtr <xsun@renttherunway.com>
2017-06-23 11:44:52 -0400

    Sql script for Aging Report

--- inventory_report_aging_daily.sql
+++ inventory_report_aging_daily.sql
@@ -1,8 +1,9 @@
 /*get the next updated_on and location_station for each seq(action)*/
-DROP TABLE IF EXISTS temp;
-create table temp as 
+DROP TABLE IF EXISTS inventory_unit_next_action;
+create table inventory_unit_next_action as 
 select  iu.id
 ,iu.seq
+,iu.status
 ,iu.barcode
 ,iu.updated_on as cur_updated_on
 ,coalesce(LEAD(iu.updated_on) OVER(PARTITION BY iu.id order by iu.seq),getdate()) as next_updated_on
@@ -14,30 +15,35 @@ select  iu.id
 ,(iu.updated_on+ interval '48 HOUR') as aging_date
 from etl.inventory_unit as iu;
 
+
+
 /*Downsize target table with filters on location_site, aging over 2 days and updated_on date*/
-DROP TABLE IF EXISTS temp1;
-create table temp1 as 
-select temp.* 
-, DATEDIFF(hour, temp.cur_updated_on, temp.next_updated_on) as time_to_next_scan
-, ius.seq as last_seq
-from temp
-left join etl.inventory_unit_seq as ius
-on temp.id = ius.unit_id
+DROP TABLE IF EXISTS inventory_unit_next_action_over48;
+create table inventory_unit_next_action_over48 as 
+select inventory_unit_next_action.* 
+, DATEDIFF(hour, inventory_unit_next_action.cur_updated_on, inventory_unit_next_action.next_updated_on) as time_to_next_scan
+from inventory_unit_next_action
 where next_updated_on >= (cur_updated_on+ interval '48 HOUR')
-and temp.next_updated_on >='2017-01-01';
+and inventory_unit_next_action.next_updated_on >='2017-01-01'
+and status <> 'INACTIVE';
+
 
 /*Join rtrbi.dates table to get each single date within aging period of each unit*/
-DROP TABLE IF EXISTS temp2;
-create table temp2 as 
-select temp1.*
+DROP TABLE IF EXISTS aging_report_table;
+create table aging_report_table as 
+select inventory_unit_next_action_over48.*
 ,dates.fiscal_week
 ,dates.fiscal_week_start
 ,dates.fiscal_week_end
 ,dates.asofdate
-from temp1
-left join rtrbi.dates as dates
-on (dates.asofdate + interval '10 HOUR') >= temp1.aging_date
-and (dates.asofdate + interval '10 HOUR') <= temp1.next_updated_on;
+from inventory_unit_next_action_over48
+inner join rtrbi.dates as dates
+on (dates.asofdate + interval '10 HOUR') >= inventory_unit_next_action_over48.aging_date
+and (dates.asofdate + interval '10 HOUR') <= inventory_unit_next_action_over48.next_updated_on
+where cur_location_station not in ('CUSTOMER','RACK','DARK_STORAGE')
+and asofdate < CURRENT_DATE
+and (regexp_like(barcode, '^9') or regexp_like(barcode, '^1')) ;
+
 
 
 /*Summarize target table by asofdate, location_station and aging days bins*/
@@ -48,22 +54,15 @@ count(CASE WHEN time_to_next_scan>=72 AND time_to_next_scan<240 THEN 1 END) AS '
 count(CASE WHEN time_to_next_scan>=240 AND time_to_next_scan<720 THEN 1 END) AS '10-30 days',
 count(CASE WHEN time_to_next_scan>=720 AND time_to_next_scan<4320 THEN 1 END) AS '30-180 days',
 count(CASE WHEN time_to_next_scan>=4320 THEN 1 END) AS '180 days and above'
-,asofdate,cur_location_station
-from temp2
+,asofdate,cur_location_site
+from aging_report_table
 where asofdate is not null
-group by asofdate,cur_location_station
-order by asofdate,cur_location_station;
-
-select count(*)
-,asofdate
-from temp2
-where asofdate is not null
-and asofdate >'2017-01-01'
-group by asofdate
-order by asofdate;
+group by asofdate,cur_location_site
+order by asofdate DESC;
 
 
-drop table if exists temp;
-drop table if exists temp1;
 
 
+
+drop table if exists inventory_unit_next_action;
+drop table if exists inventory_unit_next_action_over48;
